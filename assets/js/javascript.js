8d58654662f200e6522fa92fef68295862a6d7bd
Your name <tylershifflett@Tylers-MacBook-Pro.local>
2022-04-09 23:15:33 -0400

    Variables Addition

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,8 @@
 // Creating Variable for Weather Dashboard 
+var weatherOneCallQueryBaseUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+var geocoder = new google.maps.geocoder;
+var place = "";
+var cityNameDisplay = "";
+var currentCityData = [];
+var currentCityGeo = [];
+var searchCities = [];
